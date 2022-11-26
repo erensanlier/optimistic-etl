06a8affaeee24240685ca74daad4726646d480c0
Deniz Surmeli <denizsurmeli@icloud.com>
2022-11-26 14:33:04 +0300

    fix(tests): fix tests for erc721 extractor

--- tests/ethereumetl/service/test_erc721_transfer_extractor.py
+++ tests/ethereumetl/service/test_erc721_transfer_extractor.py
@@ -31,11 +31,12 @@ def test_extract_transfer_from_receipt_log():
     log = EthReceiptLog()
     log.address = '0x25c6413359059694A7FCa8e599Ae39Ce1C944Da2'
     log.block_number = 1061946
-    log.data = '0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc18'
+    log.data = '0x'
     log.log_index = 0
     log.topics = ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
                   '0x000000000000000000000000e9eeaec75883f0e389a78e2260bfac1776df2f1d',
-                  '0x0000000000000000000000000000000000000000000000000000000000000000']
+                  '0x0000000000000000000000000000000000000000000000000000000000000000',
+                  '0xfad']
     log.transaction_hash = '0xd62a74c7b04e8e0539398f6ba6a5eb11ad8aa862e77f0af718f0fad19b0b0480'
 
     erc721_transfer = erc721_transfer_extractor.extract_transfer_from_log(log)
@@ -43,6 +44,7 @@ def test_extract_transfer_from_receipt_log():
     assert erc721_transfer.token_address == '0x25c6413359059694a7fca8e599ae39ce1c944da2'
     assert erc721_transfer.from_address == '0xe9eeaec75883f0e389a78e2260bfac1776df2f1d'
     assert erc721_transfer.to_address == '0x0000000000000000000000000000000000000000'
-    assert erc721_transfer.token_id == 115792089237316195423570985008687907853269984665640564039457584007913129638936
+    assert erc721_transfer.token_id == 4013 #hex(0xfad) = 4013
     assert erc721_transfer.transaction_hash == '0xd62a74c7b04e8e0539398f6ba6a5eb11ad8aa862e77f0af718f0fad19b0b0480'
     assert erc721_transfer.block_number == 1061946
+test_extract_transfer_from_receipt_log()
