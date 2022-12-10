8dd480c4411aac0794a25da7d5a221415bd54098
erensanlier <erensanlier@gmail.com>
2022-12-10 15:01:14 +0300

    dependency fix

--- setup.py
+++ setup.py
@@ -32,9 +32,6 @@ setup(
     python_requires='>=3.7.2,<4',
     install_requires=[
         'web3>=5.29,<6',
-        'eth-utils==1.10',
-        'eth-abi==2.1.1',
-        # TODO: This has to be removed when "ModuleNotFoundError: No module named 'eth_utils.toolz'" is fixed at eth-abi
         'python-dateutil>=2.8.0,<3',
         'click==8.0.4',
         'ethereum-dasm==0.1.4',
